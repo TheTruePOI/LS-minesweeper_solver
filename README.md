2303d244a547d21a210ea6264313fc2af16a3640
Chinmay Khokar <chinmay.khokar123@gmail.com>
2023-07-21 16:27:19 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# LS-minesweeper_solver
+# LS-Tools-for-Cyber-Security Project
+
+## Author
+> Chinmay Khokar | 22B1027
+
+## Part-1: Minesweeper Solver
+
+### Flag
+> flag{wh04m1_15_pr0ud_0f_y0u}
+
+### Relevant files in the repository
+- minesweeper
+- Untitled.ipynb
+- script.py
+
+## Part-2: LS 9.11 Emulator
+
+### Relevant files in the repository
+- emulator.c
---
+++ script.py
@@ -0,0 +1,492 @@
+# flag : flag{wh04m1_15_pr0ud_0f_y0u}
+
+import subprocess
+import numpy as np
+import subprocess
+
+# Define a function to handle interactive communication
+def interact_with_executable():
+    executable_path = './minesweeper'
+
+    # Start the process and create pipes for communication
+    process = subprocess.Popen(
+        executable_path,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    while True:
+        # Wait for the executable to output some data
+        output = []
+        for i in range(10):
+            output_line = process.stdout.readline().strip()
+            
+            if (output_line[0] == "G"):
+                output_line = process.stdout.readline().strip()
+            
+            print(output_line)
+            output.append(output_line)
+        # Process the output, if needed, to decide the next input
+        next_input = process_output_and_get_input(output)
+        print(next_input)
+        print("\n\n\n")
+        if (next_input == "break"):
+            break
+        
+        # Send the next input to the executable
+        process.stdin.write(next_input + '\n')
+        process.stdin.flush()
+
+    # Close the standard input of the process
+    process.stdin.close()
+
+# i --> row  |  j --> column
+def output_to_array(output):
+    gamestate = np.full((9,9), -1, dtype=int)
+    
+    for i in range(9):
+        for j in range(17):
+            
+            if (j%2 == 0):
+                char = output[i][j]
+                if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]:
+                    
+                    gamestate[i][int(j/2)] = char
+    
+    return gamestate
+
+
+# ## Gamestate array scheme
+# 
+# - -1 --> Unknown
+# - -2 --> Mine
+# - 0 to 8 --> Number of adjacent mines
+
+
+# Sorry for the messy structure
+
+# Returns an int
+def adjacent_unknowns(gamestate, i, j):
+    count = 0
+    if (i == 0):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                count += 1
+            if (gamestate[i+1][j+1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+            if (gamestate[i+1][j+1] == -1):
+                count += 1
+            if (gamestate[i][j+1] == -1):
+                count += 1
+    elif (i == 8):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                count += 1
+            if (gamestate[i-1][j+1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+            if (gamestate[i-1][j+1] == -1):
+                count += 1
+            if (gamestate[i][j+1] == -1):
+                count += 1
+    else:
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                count += 1
+            if (gamestate[i+1][j+1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+            if (gamestate[i-1][j+1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+            if (gamestate[i-1][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j-1] == -1):
+                count += 1
+            if (gamestate[i+1][j] == -1):
+                count += 1
+            if (gamestate[i+1][j+1] == -1):
+                count += 1
+            if (gamestate[i-1][j-1] == -1):
+                count += 1
+            if (gamestate[i-1][j] == -1):
+                count += 1
+            if (gamestate[i-1][j+1] == -1):
+                count += 1
+            if (gamestate[i][j+1] == -1):
+                count += 1
+                
+    return (count)
+
+
+# Returns an int
+def adjacent_mines(gamestate, i, j):
+    count = 0
+    if (i == 0):
+        if (j == 0):
+            if (gamestate[i][j+1] == -2):
+                count += 1
+            if (gamestate[i+1][j+1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+            if (gamestate[i+1][j+1] == -2):
+                count += 1
+            if (gamestate[i][j+1] == -2):
+                count += 1
+    elif (i == 8):
+        if (j == 0):
+            if (gamestate[i][j+1] == -2):
+                count += 1
+            if (gamestate[i-1][j+1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+            if (gamestate[i-1][j+1] == -2):
+                count += 1
+            if (gamestate[i][j+1] == -2):
+                count += 1
+    else:
+        if (j == 0):
+            if (gamestate[i][j+1] == -2):
+                count += 1
+            if (gamestate[i+1][j+1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+            if (gamestate[i-1][j+1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+        elif (j == 8):
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+            if (gamestate[i-1][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+        else:
+            if (gamestate[i][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j-1] == -2):
+                count += 1
+            if (gamestate[i+1][j] == -2):
+                count += 1
+            if (gamestate[i+1][j+1] == -2):
+                count += 1
+            if (gamestate[i-1][j-1] == -2):
+                count += 1
+            if (gamestate[i-1][j] == -2):
+                count += 1
+            if (gamestate[i-1][j+1] == -2):
+                count += 1
+            if (gamestate[i][j+1] == -2):
+                count += 1
+                
+    return (count)
+
+
+def mark_mines(gamestate, i, j):
+    # Using the same base structure as adjacent_unknowns
+    
+    if (i == 0):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+            if (gamestate[i+1][j+1] == -1):
+                gamestate[i+1][j+1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i+1][j-1] == -1):
+                gamestate[i+1][j-1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+        else:
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i+1][j-1] == -1):
+                gamestate[i+1][j-1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+            if (gamestate[i+1][j+1] == -1):
+                gamestate[i+1][j+1] = -2
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+    elif (i == 8):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+            if (gamestate[i-1][j+1] == -1):
+                gamestate[i-1][j+1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i-1][j-1] == -1):
+                gamestate[i-1][j-1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+        else:
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i-1][j-1] == -1):
+                gamestate[i-1][j-1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+            if (gamestate[i-1][j+1] == -1):
+                gamestate[i-1][j+1] = -2
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+    else:
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+            if (gamestate[i+1][j+1] == -1):
+                gamestate[i+1][j+1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+            if (gamestate[i-1][j+1] == -1):
+                gamestate[i-1][j+1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i+1][j-1] == -1):
+                gamestate[i+1][j-1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+            if (gamestate[i-1][j-1] == -1):
+                gamestate[i-1][j-1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+        else:
+            if (gamestate[i][j-1] == -1):
+                gamestate[i][j-1] = -2
+            if (gamestate[i+1][j-1] == -1):
+                gamestate[i+1][j-1] = -2
+            if (gamestate[i+1][j] == -1):
+                gamestate[i+1][j] = -2
+            if (gamestate[i+1][j+1] == -1):
+                gamestate[i+1][j+1] = -2
+            if (gamestate[i-1][j-1] == -1):
+                gamestate[i-1][j-1] = -2
+            if (gamestate[i-1][j] == -1):
+                gamestate[i-1][j] = -2
+            if (gamestate[i-1][j+1] == -1):
+                gamestate[i-1][j+1] = -2
+            if (gamestate[i][j+1] == -1):
+                gamestate[i][j+1] = -2
+
+
+# Returns tuple (i,j)
+def find_adjacent_unknown(gamestate, i, j):
+    if (i == 0):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+            if (gamestate[i+1][j+1] == -1):
+                return  ((i+1, j+1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i+1][j-1] == -1):
+                return  ((i+1, j-1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+        else:
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i+1][j-1] == -1):
+                return  ((i+1, j-1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+            if (gamestate[i+1][j+1] == -1):
+                return  ((i+1, j+1))
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+    elif (i == 8):
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+            if (gamestate[i-1][j+1] == -1):
+                return  ((i-1, j+1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i-1][j-1] == -1):
+                return  ((i-1, j-1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+        else:
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i-1][j-1] == -1):
+                return  ((i-1, j-1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+            if (gamestate[i-1][j+1] == -1):
+                return  ((i-1, j+1))
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+    else:
+        if (j == 0):
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+            if (gamestate[i+1][j+1] == -1):
+                return  ((i+1, j+1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+            if (gamestate[i-1][j+1] == -1):
+                return  ((i-1, j+1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+        elif (j == 8):
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i+1][j-1] == -1):
+                return  ((i+1, j-1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+            if (gamestate[i-1][j-1] == -1):
+                return  ((i-1, j-1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+        else:
+            if (gamestate[i][j-1] == -1):
+                return  ((i, j-1))
+            if (gamestate[i+1][j-1] == -1):
+                return  ((i+1, j-1))
+            if (gamestate[i+1][j] == -1):
+                return  ((i+1, j))
+            if (gamestate[i+1][j+1] == -1):
+                return  ((i+1, j+1))
+            if (gamestate[i-1][j-1] == -1):
+                return  ((i-1, j-1))
+            if (gamestate[i-1][j] == -1):
+                return  ((i-1, j))
+            if (gamestate[i-1][j+1] == -1):
+                return  ((i-1, j+1))
+            if (gamestate[i][j+1] == -1):
+                return  ((i, j+1))
+
+    return  ((69, 69))
+
+
+# Define a function to process the output and decide the next input
+
+# Has to return string of format "i,j"
+def process_output_and_get_input(output):
+    gamestate = output_to_array(output)
+    
+    # Run-through, marking mines
+    for i in range(9):
+        for j in range(9):
+            if (gamestate[i][j] == (adjacent_unknowns(gamestate, i, j) + adjacent_mines(gamestate, i, j))):
+                mark_mines(gamestate, i, j)
+    print(gamestate)
+    # Run-through, trying to find a safe move
+    for i in range(9):
+        for j in range(9):
+            if (gamestate[i][j] == adjacent_mines(gamestate, i, j)):
+                valid_move = find_adjacent_unknown(gamestate, i, j)
+                if (valid_move[0] != 69):
+                    return(f"{valid_move[0]},{valid_move[1]}")
+    
+    # Return a random valid move
+    if ((gamestate == np.full((9,9), -1, dtype=int)).sum() == 81):
+        return("2,4")
+
+    return ("break")
+
+
+# Call the function
+interact_with_executable()
+
